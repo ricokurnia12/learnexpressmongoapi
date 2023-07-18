0d8edc1c1ec4561c4eb049ef8561d5d9901889af
ricokurnia12 <ricokurnia19@gmail.com>
2023-07-18 16:18:45 +0700

    adding controllers

---
+++ controllers/index.js
@@ -0,0 +1,62 @@
+const Product = require('../models/productModels.js');
+
+module.exports = {
+  getAllProduct: async (req, res) => {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (error) {}
+  },
+  getProudctById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  addProduct: async (req, res) => {
+    try {
+      const products = await Product.create(req.body);
+      res.status(200).json(products);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  editProduct : async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body);
+      // cannot find a product
+      if (!product) {
+        return res
+          .status(404)
+          .json({ message: `can't find product with id: ${id}` });
+      }
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  },
+  deleteProduct : async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findByIdAndDelete(id);
+      if (!product) {
+        return res
+          .status(404)
+          .json({ message: `can't find product with id: ${id}` });
+      }
+  
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: error.message });
+    }
+  }
+};
