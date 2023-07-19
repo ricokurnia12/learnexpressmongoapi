c7fa4f610d931dff4852578de075ed27869c83fe
COCO <ricokurnia19@gmail.com>
2023-07-19 21:12:24 +0700

    finishing services

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,11 @@
 // const Product = require('../models/productModels.js');
-const { findAllProducts } = require('../services');
+const {
+  findAllProducts,
+  findProductById,
+  addingProduct,
+  editProductById,
+  deleteProductById,
+} = require('../services');
 
 module.exports = {
   getAllProduct: async (req, res) => {
@@ -13,7 +19,7 @@ module.exports = {
   getProudctById: async (req, res) => {
     try {
       const { id } = req.params;
-      const product = await Product.findById(id);
+      const product = await findProductById(id);
       res.status(200).json(product);
     } catch (error) {
       console.log(error.message);
@@ -22,7 +28,7 @@ module.exports = {
   },
   addProduct: async (req, res) => {
     try {
-      const products = await Product.create(req.body);
+      const products = await addingProduct(req.body);
       res.status(200).json(products);
     } catch (error) {
       console.log(error.message);
@@ -32,24 +38,18 @@ module.exports = {
   editProduct: async (req, res) => {
     try {
       const { id } = req.params;
-      const product = await Product.findByIdAndUpdate(id, req.body);
-      // cannot find a product
-      if (!product) {
-        return res
-          .status(404)
-          .json({ message: `can't find product with id: ${id}` });
-      }
-      const updatedProduct = await Product.findById(id);
-      res.status(200).json(updatedProduct);
+      const product = await editProductById(id, req.body, res);
+
+      res.status(200).json(product);
     } catch (error) {
       console.log(error.message);
-      res.status(500).json({ message: error.message });
+      res.status(500).json('cant find product');
     }
   },
   deleteProduct: async (req, res) => {
     try {
       const { id } = req.params;
-      const product = await Product.findByIdAndDelete(id);
+      const product = await deleteProductById(id);
       if (!product) {
         return res
           .status(404)
--- services/index.js
+++ services/index.js
@@ -3,12 +3,46 @@ const Product = require('../models/productModels.js');
 module.exports = {
   findAllProducts: async () => {
     try {
-      const products = await Product.find();
-      console.log(products, '>>> all product');
-
-      return products;
+      return await Product.find();
     } catch (error) {
       throw error;
     }
   },
+  findProductById: async (id) => {
+    try {
+      return await Product.findById(id);
+    } catch (error) {
+      throw error;
+    }
+  },
+  addingProduct: async (body) => {
+    try {
+      return await Product.create(body);
+    } catch (error) {
+      throw error;
+    }
+  },
+  editProductById: async (id, body, res) => {
+    try {
+       await Product.findByIdAndUpdate(id, body);
+      // cannot find a product
+      // if (!product) {
+      //   return res
+      //     .status(500)
+      //     .json({ message: `can't find product with id: ${id}` });
+      // }
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      throw error;
+    }
+  },
+  deleteProductById :  async (id) =>{
+    try {
+      return await Product.findByIdAndDelete(id);
+    } catch (error) {
+      throw error
+    }
+  }
+
 };
