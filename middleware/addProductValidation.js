6c1b800e3a71a7e09f538c072288a38316179514
ricokurnia12 <ricokurnia19@gmail.com>
2023-07-21 17:21:11 +0700

    add middleware

---
+++ middleware/addProductValidation.js
@@ -0,0 +1,17 @@
+const addProductValidation = (req, res, next) => {
+  const { quantity, name, price } = req.body;
+  //   quantity < 1 || name.length < 3 || price < 1000;
+  
+
+  if (quantity < 1 || quantity === null ) {
+    res.status(500).json('quantity minimal is 1');
+  } else if (name.length < 3) {
+    res.status(500).json('character minimal is 3');
+  } else if (price < 1000) {
+    res.status(500).json('price minimal is 1000');
+  } else {
+    next();
+  }
+};
+
+module.exports = addProductValidation;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,20 +5,19 @@ const {
   getProudctById,
   editProduct,
   deleteProduct,
-  getPagination
+  getPagination,
 } = require('../controllers/productControllers');
-
+const addProductValidation = require('../middleware/addProductValidation.js');
 
 router.get('/allproducts', getAllProduct);
-router.get('/pagination', getPagination)
+router.get('/pagination', getPagination);
 
 router.get('/:id', getProudctById);
 
-router.post('/addproduct', addProduct);
+router.post('/addproduct', addProductValidation, addProduct);
 
 router.put('/:id', editProduct);
 
 router.delete('/:id', deleteProduct);
-
 
 module.exports = router;
